a126724e1fb4982bf80504ce569e5275475650cb
pooolina1616 <pooolina1616@gmail.com>
2021-12-27 20:55:43 +0300

    final

--- task_final/solution.js
+++ task_final/solution.js
@@ -34,20 +34,13 @@
 // }
 
 function sendRequest(name, phone, address, goods, sum) {
-    let data = {goods: [], order: {}};
+    const data = {client: '', order: {}, goods: []};
 
-    let countOfGoods = goods.length;
+    goods.forEach(item => data.goods.push({title: item.title, count: item.count}));
 
-    for (let i = 0; i <= countOfGoods; i += 1) {
-        data.goods.push(goods[i].title);
-    }
+    data.order.address = `ул. ${address.street}, дом ${address.house}, ${address.entrance} подъезд, ${address.floor} этаж, кв ${address.flat}`;
+    data.order.sum = sum;
+    data.client = `${name} ${phone}`;
 
-    data.order.address = address;
-    data.order.sum = name + phone + address + goods + sum;
-
-    data.client = 'Иван';
-
-    let jsonData = JSON.stringify(data);
-
-    return jsonData;
+    return JSON.stringify({data});
 }
